b53c367168a65980246bcfbb6316fd0cfd06a2c8
Elena Semseeva <e.semseeva@yandex.ru>
2023-10-06 15:44:56 +0300

    style: delete unused import

--- src/main/java/ru/practicum/shareit/booking/dto/RequestBookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/RequestBookingDto.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking.dto;
 
 import lombok.Data;
-import ru.practicum.shareit.user.model.User;
 
 import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
